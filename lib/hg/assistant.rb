fed2496ebda3eba37429790e60c1950a940715f0
Matt Buck <github@matthewbuck.com>
2017-04-16 13:27:06 -0500

    Enable proper autoloading

--- lib/hg/assistant.rb
+++ lib/hg/assistant.rb
@@ -1,11 +1,8 @@
 require 'dry-configurable'
 
 require 'hg/assistant/engine'
-require 'hg/assistant/bot'
-require 'hg/assistant/controller'
 
 module Hg
   module Assistant
-    # Your code goes here...
   end
 end
--- lib/hg/assistant/engine.rb
+++ lib/hg/assistant/engine.rb
@@ -1,6 +1,10 @@
 module Hg
   module Assistant
     class Engine < ::Rails::Engine
+      AUTOLOAD_PATHS = Dir.glob(File.join(Hg::Assistant::Engine.root, 'lib', '**/'))
+      config.autoload_paths   += AUTOLOAD_PATHS
+      config.eager_load_paths += AUTOLOAD_PATHS
+
       isolate_namespace Hg::Assistant
     end
   end
