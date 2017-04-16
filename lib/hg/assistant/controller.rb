75dfca28beb1af594e966af656dcb2d2ddf109cb
Matt Buck <github@matthewbuck.com>
2017-04-16 15:40:48 -0500

    Clean up speech and display responses

--- lib/hg/assistant/controller.rb
+++ lib/hg/assistant/controller.rb
@@ -2,14 +2,27 @@ module Hg
   module Assistant
     # TODO: Move Messenger-specific behavior out of base controller class.
     class Controller < ::Hg::Controller
+      include ActionView::Helpers::SanitizeHelper
+
       attr_accessor :response
 
       # @see https://developers.google.com/actions/reference/webhook-format
       # TODO: allow specifying reprompts
       def respond(speech_response, display_text: nil)
+        # Clean up speech responses: fix heredocs, remove linebreaks,
+        # fix multiple spaces.
+        speech_response = speech_response.strip_heredoc.squish
+
+        # Use the speech response as the display text, if none provided.
+        display_text = speech_response || display_text
+
+        # Remove any SSML tags from the display text.
+        display_text = strip_tags(display_text).squish
+
+        # Generate a proper webhook fulfillment response object.
         {
-          speech:               speech_response,
-          display_text:         (speech_response || display_text),
+          speech:       speech_response,
+          display_text: display_text,
           data: {
             google: {
               expect_user_response: false
